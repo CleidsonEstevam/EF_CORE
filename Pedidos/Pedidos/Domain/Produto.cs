3cc0c59d299c57682a9c869412b55013512feb0d
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-02-22 20:56:29 -0300

    TabelasMapeadasComFluentAPI

--- Pedidos/Pedidos/Domain/Produto.cs
+++ Pedidos/Pedidos/Domain/Produto.cs
@@ -9,7 +9,7 @@ namespace Pedidos.Domain
     {
         public int Id { get; set; }
         public string CodigoDeBarras { get; set; }
-        public string  Descrição { get; set; }
+        public string  Descricao { get; set; }
         public decimal Valor { get; set; }
         public TipoProduto TipoProduto { get; set; }
         public bool Ativo { get; set; }
