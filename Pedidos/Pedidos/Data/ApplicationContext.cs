6ab8bc4d584cd03dac323a4be1ce1bbff1251886
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-03-01 19:03:39 -0300

    InsertDeRegistro

--- Pedidos/Pedidos/Data/ApplicationContext.cs
+++ Pedidos/Pedidos/Data/ApplicationContext.cs
@@ -6,7 +6,10 @@ namespace Pedidos.Data
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Pedido>  Pedidos  { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
         {
             optionsBuilder.UseSqlServer("Data Source=CAsa\\SQLEXPRESS;Initial Catalog=Pedidos; Integrated Security=True");
--- Pedidos/Pedidos/Program.cs
+++ Pedidos/Pedidos/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Pedidos.Domain;
+using Pedidos.Enums;
+using System;
 
 namespace Pedidos
 {
@@ -6,7 +9,32 @@ namespace Pedidos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoDeBarras = "12345678910",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaRevenda,
+                Ativo = true
+            };
+            //opções para inserir registros com entity
+            using var db = new Data.ApplicationContext();
+            //1:
+            //db.Produtos.Add(produto);
+            //2:Metodo generico
+            //db.Set<Produto>().Add(produto);
+            //3: Forçando rastreio
+            //db.Entry(produto).State = EntityState.Added;
+            //4:
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine(registros);
         }
     }
 }
