dc7f1a175aa2981d9d6d4a38a4a9ebc19e74e4a8
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-03-02 20:06:57 -0300

    SelectEmEntidadesRelacionadas

--- Pedidos/Pedidos/Program.cs
+++ Pedidos/Pedidos/Program.cs
@@ -2,6 +2,7 @@
 using Pedidos.Domain;
 using Pedidos.Enums;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Pedidos
@@ -12,7 +13,8 @@ namespace Pedidos
         {
             //InserirDados();
             //InserirDadosEmMassa();
-            ConsultarDados();
+            //ConsultarDados();
+            //CadastrarPedido();
         }
 
         #region "Insert"
@@ -89,9 +91,48 @@ namespace Pedidos
             var registros = db.SaveChanges();
             Console.WriteLine(registros);
         }
+
+        private static void CadastrarPedido() 
+        {
+            //Instancia do contex
+            using var db = new Data.ApplicationContext();
+            //Indo no banco e pegando o primeiro cliente e produto que encontrar
+            var cliente = db.Clientes.FirstOrDefault();   
+            var produto = db.Produtos.FirstOrDefault();
+
+            // Juntando as duas consultas
+            //Criando Pedido 
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                DataInicio = DateTime.Now,
+                DataFim = DateTime.Now,
+                Observacao = "Pedido teste",
+                Status = StatusDoPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+
+                Items = new List<PedidoItem>
+                {
+                    new PedidoItem
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10,
+                    }
+
+                }
+            };
+            db.Pedidos.Add(pedido);
+
+            db.SaveChanges();
+        }
         #endregion
 
         #region "Consultar"
+        /// <summary>
+        /// Consulta Simples
+        /// </summary>
         private static void ConsultarDados() 
         {
             using var db = new Data.ApplicationContext();
@@ -111,6 +152,31 @@ namespace Pedidos
             }
 
         }
+        /// <summary>
+        /// Consulta adiatada
+        /// </summary>
+        private static void ConsultarPedidoCarregamentoAdiantado() 
+        {
+            using var db = new Data.ApplicationContext();
+
+            //Nesse caso só vai ser mapeado a propriedade pedido
+            //var pedidos = db.Pedidos.ToList();
+
+            //Nesse caso sera pedido e incluso vira os Items
+            //var pedidos = db.Pedidos.Include(p => p.Items).ToList();
+
+            //Nesse cado sera incluso os items e os produtos
+            var pedidos = db.Pedidos
+                .Include(p => p.Items)
+                .ThenInclude(p => p.Produto)
+                .ToList();
+
+
+
+            Console.WriteLine(pedidos.Count);
+        }
         #endregion
+
+
     }
 }
